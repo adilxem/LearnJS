6d5ffb549108724f16fe3a786bd5628903c710e9
adil <adil.shakeel104@gmail.com>
2024-06-29 16:44:41 +0530

    hoisting in JS

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+// hoisting general meaning: to lift or pull something up
+
+getName();
+console.log(x);
+console.log(getName);
+
+
+var x = 7;
+
+function getName() {
+    console.log("hello");
+}
+
+// when the execution context is created, it allocates memory to the variables and functions first and then assignes the values in the code section of the execution context. that is why while executing the above code, getName() shows the output because it is completely stored in the memory; but the value of x is 'undefined' because it has been allocated space in the memory but the value '7' is not yet assigned to the variable 'x'
