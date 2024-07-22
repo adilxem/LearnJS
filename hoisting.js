aef0ff3db0fc4ef50fbb7d9e2f768e802185e654
adil <122447806+adilxem@users.noreply.github.com>
2024-07-22 18:23:58 +0530

    hoisting with functions as variables
    added arrow functions to demonstrate how hoisting works in execution context with functions declared as variables

--- hoisting.js
+++ hoisting.js
@@ -4,6 +4,9 @@ getName();
 console.log(x);
 console.log(getName);
 
+console.log(hi); //output: undefined
+
+hi();
 
 var x = 7;
 
@@ -12,3 +15,13 @@ function getName() {
 }
 
 // when the execution context is created, it allocates memory to the variables and functions first and then assignes the values in the code section of the execution context. that is why while executing the above code, getName() shows the output because it is completely stored in the memory; but the value of x is 'undefined' because it has been allocated space in the memory but the value '7' is not yet assigned to the variable 'x'
+
+var hi = () => {
+
+    console.log("hi");
+}
+
+console.log(hi);
+hi();
+
+// try executing...
