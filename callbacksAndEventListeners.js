24d9861043e295a9120fcb44e60e0cd0f94df281
adil <122447806+adilxem@users.noreply.github.com>
2024-07-25 23:42:03 +0530

    Deleted callbacksAndEventListeners

--- callbacksAndEventListeners.js
+++
@@ -1,5 +0,0 @@
-// What is a Callback Function?
-
-// A callback function is a function that is passed as an argument to another function, to be “called back” at a later time.
-
-// JavaScript is a synchronous single-threaded language; butt w/ callbacks, we can do asynchronous things in JS
